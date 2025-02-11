1b625e39472f274c3db3b1e1f5a24955b24af49e
Akshay  Kanawade <71754780+Akshay-Kanawade@users.noreply.github.com>
2025-02-11 05:43:41 +0000

    11-feb

---
+++ Basic/pathmodule.js
@@ -0,0 +1,6 @@
+const path = require('path')
+const Path = require('path')
+
+console.log('filemname',path.basename(__filename))
+console.log('dirname',path.dirname(__filename))
+console.log('extname',path.extname(__filename))
